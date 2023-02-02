0bb098f68215b3f5b67ea0006d814f0cbb1fce47
jeannedarcsagna <jeanne97sagna@gmail.com>
2023-02-02 16:56:08 +0100

    first commit

---
+++ child-project-1/src/test/java/group5AWS/com/AppTest.java
@@ -0,0 +1,20 @@
+package group5AWS.com;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ src/main/java/group5AWS/com/App.java
@@ -0,0 +1,13 @@
+package group5AWS.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
